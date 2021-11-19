e05e5454b464e9c8ec601b338a767ff4843ddfe3
Vexnos <88638499+Vexnos@users.noreply.github.com>
2021-11-20 11:48:50 +1300

    Initial commit

---
+++ music.py
@@ -0,0 +1,91 @@
+'''
+
+    music.py
+
+    Author: Vexnos
+    Date: 2021-08-08
+
+    VexBot Music Dictionary
+
+'''
+
+#Music Dictionary
+music = {
+    "oceanview": ["music/OceanView.mp3", 1, "Patrick Patrikios", "Oceanview"],
+    "cover": ["music/Cover.mp3", 1, "Patrick Patrikios", "Cover"],
+    "enthusiasm": ["music/curb_you_enthusiasm.mp3", 1, "Patrick Patrikios", "Curb your Enthusiasm Theme"],
+    "alaska": ["music/Alaska-Sky.mp3", 1, "Patrick Patrikios", "Alaska Sky"],
+    "hot-coffee": ["music/Hot-Coffee.mp3", 1, "Patrick Patrikios", "Hot Coffee"],
+    "awakened": ["music/awakened.mp3", 1, "Andreas Waldetoft", "Awakened"],
+    "true-hero": ["music/BAaTH.mp3", 1, "Tobyfox", "Battle Against a True Hero"],
+    "core": ["music/CORE.mp3", 1, "Tobyfox", "Core"],
+    "creation": ["music/creation.mp3", 1, "Andreas Waldetoft", "Creation"],
+    "glamour": ["music/DeathByGlamour.mp3", 1, "Tobyfox", "Death by Glamour"],
+    "travels": ["music/deepspacetravels.mp3", 1, "Andreas Waldetoft", "Deep Space Travels"],
+    "ghost": ["music/GhostFight.mp3", 1, "Tobyfox", "Ghost Fight"],
+    "hopes": ["music/HopesAndDreams.mp3", 1, "Tobyfox", "Hopes and Dreams"],
+    "lastgoodbye": ["music/LastGoodbye.mp3", 1, "Tobyfox", "Last Goodbye"],
+    "laststand": ["music/laststand.mp3", 1, "Andreas Waldetoft", "Last Stand"],
+    "megalovania": ["music/Megalovania.mp3", 1, "Tobyfox", "Megalovania"],
+    "respite": ["music/Respite.mp3", 1, "Tobyfox", "Respite"],
+    "ruins": ["music/Ruins.mp3", 1, "Tobyfox", "Ruins"],
+    "save": ["music/SAVE.mp3", 1, "Tobyfox", "Save The World"],
+    "salutations": ["music/Salutations.mp3", 1, "Simon Chylinski", "Salutations"],
+    "unknown": ["music/Unknown.mp3", 1, "Simon Chylinski", "Unknown"],
+    "godrays": ["music/GodRays.mp3", 1, "Simon Chylinski", "Godrays"],
+    "sunmoon": ["music/SunMoon.mp3", 1, "Simon Chylinski", "Sun & Moon"],
+    "precipe": ["music/Precipe.mp3", 1, "Simon Chylinski", "Precipe"],
+    "leviathan": ["music/Leviathan.mp3", 1, "Simon Chylinski", "Leviathan"],
+    "inhabitants": ["music/Inhabitants.mp3", 1, "Simon Chylinski", "Inhabitants"],
+    "abandon": ["music/AbandonShip.mp3", 1, "Simon Chylinski", "Abandon Ship"],
+    "asgore": ["music/Asgore.mp3", 1, "Tobyfox", "Asgore"],
+    "dogsong": ["music/Dogsong.mp3", 1, "Tobyfox", "Dog song"],
+    "snowdin": ["music/Snowdin.mp3", 1, "Tobyfox", "Snowdin"],
+    "bonetrousle": ["music/Bonetrousle.mp3", 1, "Tobyfox", "Bonetrousle"],
+    "thundersnail": ["music/Thundersnail.mp3", 1, "Tobyfox", "Thundersnail"],
+    "stronger-monsters": ["music/Stronger_Monsters.mp3", 1, "Tobyfox", "Stronger Monsters"],
+    "waterfall": ["music/Waterfall.mp3", 1, "Tobyfox", "Waterfall"],
+    "fade-away": ["music/Fade Away.mp3", 1, "SIX60", "Fade Away"],
+    "gangnam-style": ["music/Gangnam Style.mp3", 1, "PSY", "Gangnam Style"],
+    "hbfs": ["music/HBFS.mp3", 1, "Daft Punk", "Harder, Better, Faster, Stronger"],
+    "get-lucky": ["music/Get Lucky.mp3", 1, "Daft Punk, Featuring: Pharrell Williams and Nile Rodgers", "Get Lucky"],
+    "one-more-time": ["music/One More Time.mp3", 1, "Daft Punk", "One More Time"],
+    "atw-hbfs": ["music/ATW-HBFS.mp3", 1, "Daft Punk", "Around the World / Harder, Better, Faster, Stronger"],
+    "tlsm": ["music/TooLong_SteamMachine.mp3", 1, "Daft Punk", "Too Long / Steam Machine"],
+    "tv-cres": ["music/TV-Crescendolls.mp3", 1, "Daft Punk", "Television Rules the Nation / Crescendolls"],
+    "da-funk-daft": ["music/da_funk_daftendirekt.mp3", 1, "Daft Punk", "Da Funk / Daftendirekt"],
+    "touch-epilogue": ["music/Touch Epilogue.mp3", 1, "Daft Punk", "Touch (Epilogue Edit)"],
+    "burning-toolong": ["music/burning_too_long.mp3", 1, "Daft Punk", "Burnin' / Too Long"],
+    "robotrock-ohyeah": ["music/robot-oh-yeah.mp3", 1, "Daft Punk", "Robot Rock / Oh Yeah"],
+    "digital-love": ["music/Digital Love.mp3", 1, "Daft Punk", "Digital Love"],
+    "aerodynamic": ["music/Aerodynamic.mp3", 1, "Daft Punk", "Aerodynamic"],
+    "glbtm": ["music/Give Life Back to Music.mp3", 1, "Daft Punk", "Give Life back to Music"],
+    "instant-crush": ["music/Instant Crush.mp3", 1, "Daft Punk, Featuring: Julian Casablancas", "Instant Crush"],
+    "kickstart-my-heart": ["music/Kickstart My Heart.mp3", 1, "Mötley Crüe", "Kickstart My Heart"],
+    "paper-planes": ["music/Paper Planes.mp3", 1, "MIA", "Paper Planes"],
+    "bliss": ["music/Bliss.mp3", 1, "Th' Dudes", "Bliss"],
+    "waiting-for-love": ["music/Waiting For Love.mp3", 1, "Avicii", "Waiting For Love"],
+    "city-of-tears": ["music/HollowKnight/CityOfTears.mp3", 1, "Christopher Larkin", "City of Tears"],
+    "greenpath": ["music/HollowKnight/Greenpath.mp3", 1, "Christopher Larkin", "Greenpath"],
+    "grimm": ["music/HollowKnight/Grimm.mp3", 1, "Christopher Larkin", "Grimm Troupe"],
+    "hollow-knight": ["music/HollowKnight/HollowKnight.mp3", 1, "Christopher Larkin", "Hollow Knight"],
+    "hornet": ["music/HollowKnight/Hornet.mp3", 1, "Christopher Larkin", "Hornet"],
+    "pale-court": ["music/HollowKnight/PaleCourt.mp3", 1, "Christopher Larkin", "Pale Court"],
+    "pure-vessel": ["music/HollowKnight/PureVessel.mp3", 1, "Christopher Larkin", "Pure Vessel"],
+    "radiance": ["music/HollowKnight/Radiance.mp3", 1, "Christopher Larkin", "Radiance"],
+    "sealed-vessel": ["music/HollowKnight/SealedVessel.mp3", 1, "Christopher Larkin", "Sealed Vessel"],
+    "white-palace": ["music/HollowKnight/WhitePalace.mp3", 1, "Christopher Larkin", "White Palace"],
+    "nightmare-king": ["music/HollowKnight/NightmareKing.mp3", 1, "Christopher Larkin", "Nightmare King Grimm"],
+    "broken-vessel": ["music/HollowKnight/BrokenVessel.mp3", 1, "Christopher Larkin", "Broken Vessel"],
+    "crossroads": ["music/HollowKnight/Crossroads.mp3", 1, "Christopher Larkin", "Crossroads"],
+    "crystal-peak": ["music/HollowKnight/CrystalPeak.mp3", 1, "Christopher Larkin", "Crystal Peak"],
+    "dirtmouth": ["music/HollowKnight/Dirtmouth.mp3", 1, "Christopher Larkin", "Dirtmouth"],
+    "soul-sanctum": ["music/HollowKnight/SoulSanctum.mp3", 1, "Christopher Larkin", "Soul Sanctum"],
+    "zote": ["music/HollowKnight/Zote.mp3", 1, "Christopher Larkin", "Truth, Beauty and Hatred"],
+    "drag-me-down": ["music/Drag Me Down.mp3", 1, "One Direction", "Drag Me Down"],
+    "something-about-us": ["music/Something About Us.mp3", 1, "Daft Punk", "Something About Us"]
+}
+
+# Cut music
+'''
+'''
